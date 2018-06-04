2f9aa7d5ec68fe4c2dd1bd20ded9ca587546cb1b
Michael Papadopoulos <micpap25@gmail.com>
2018-06-04 19:37:30 -0400

    Game

--- src/dinogame.java
+++ src/dinogame.java
@@ -1,4 +1,5 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
@@ -7,6 +8,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -19,10 +23,11 @@ public class dinogame extends JFrame {
         add(p);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         dinogame frame = new dinogame();
         frame.setTitle("Dino McMan");
         frame.setSize(400, 400);
+        frame.setBackground(Color.WHITE);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -39,7 +44,7 @@ class Dino extends JPanel {
     int score = 0;
     boolean haslost = false;
     int height = 20;
-    int delay=5;
+    boolean crouch=false;
     BufferedReader in;
     public static PrintWriter out;
     JLabel score1= new JLabel("Score:");
@@ -53,12 +58,15 @@ class Dino extends JPanel {
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_UP && jump < 21&&jump>10||(e.getKeyCode() == KeyEvent.VK_SPACE && jump < 21&&jump>10)) {
+                if (e.getKeyCode() == KeyEvent.VK_UP && jump < 21&&jump>10) {
                     timealoft = 25;
-                } else if (e.getKeyCode() == KeyEvent.VK_DOWN && jump > 30)
-                    jump -=10;
-                else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    delay=0;
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN && jump > 39)
+                    jump /=2;
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    crouch = true;
+                }
+                else {
+                    crouch = false;
                 }
 
             }
@@ -75,11 +83,11 @@ class Dino extends JPanel {
             else if (jump>20){
                 jump-=1;
             }
-            if(delay<5) {
+            if(crouch) {
                 height = 10;
                 jump = 10;
             }
-            else if (delay>5&&height-jump==0){
+            else if (height-jump==0){
                 height = 20;
                 jump = 20;
             }
@@ -91,17 +99,17 @@ class Dino extends JPanel {
                 for(int i =0;i<19;i++){
                     cactuslist[i]=cactuslist[i+1];
                 }
-                int p = rand.nextInt(12);
+                int p = rand.nextInt(8);
                 if(score<500&&(p==2||p==3))
-                    p=rand.nextInt(1);
+                    p=rand.nextInt(2);
                 cactuslist[19]=(new cactus(p,speed));
                 k=0;
 
 
             }
             for(int i = 0;i<cactuslist.length;i++){
-                if (getWidth()-30-cactuslist[i].x==getWidth()/10)
-                    if((cactuslist[i].type==0&&jump<40)||(cactuslist[i].type==1&&jump<30)||(cactuslist[i].type==2&&jump>25)||(cactuslist[i].type==3&&(jump<40&&jump>10))){
+                if (getWidth()-30-cactuslist[i].x<getWidth()/10+15 && getWidth()-30-cactuslist[i].x+5>getWidth()/10)
+                    if((cactuslist[i].type==0&&jump<40)||(cactuslist[i].type==1&&jump<30)||(cactuslist[i].type==2&&jump>25)||(cactuslist[i].type==3&&(jump<45&&jump>10))){
                         haslost = true;
                     }
             }
@@ -125,7 +133,6 @@ class Dino extends JPanel {
                 cactuslist[i].move();
             }
             k++;
-            delay++;
             repaint();
 
             }
@@ -138,9 +145,22 @@ class Dino extends JPanel {
         for(int i = 0;i<cactuslist.length;i++){
             cactuslist[i].paint(this,g);
         }
-        g.fillRect(getWidth()/10,getHeight()/2-jump,10,height);
+        try {
+            if (crouch && score%6>=3) {
+                BufferedImage rex = ImageIO.read(new File("images\\ChromeDino_Duck2.png"));
+                g.drawImage(rex, getWidth() / 10, getHeight() / 2 - jump, 15, height, null);
+            } else if (crouch && score%6<=2){
+                BufferedImage rex = ImageIO.read(new File("images\\ChromeDino_Duck1.png"));
+                g.drawImage(rex, getWidth() / 10, getHeight() / 2 - jump, 15, height, null);
+            } else if (score%6 >=3) {
+                BufferedImage rex = ImageIO.read(new File("images\\ChromeDino_Run2.png"));
+                g.drawImage(rex, getWidth() / 10, getHeight() / 2 - jump, 15, height, null);
+            } else {
+                BufferedImage rex = ImageIO.read(new File("images\\ChromeDino_Run1.png"));
+                g.drawImage(rex, getWidth() / 10, getHeight() / 2 - jump, 15, height, null);
+            }
 
-
+        }catch (IOException ex){};
     }
 
 }
@@ -157,17 +177,23 @@ class cactus {
     public void move(){
         x+=speed;
     }
-    protected void paint(JPanel p ,Graphics g) {
+    protected void paint(JPanel p ,Graphics g){
+        try {
+            if (type == 0) {
+                final BufferedImage image = ImageIO.read(new File("images\\ChromeDino_CactusBunch1.png"));
+                g.drawImage(image, p.getWidth() - 30 - x, p.getHeight() / 2 - 12, 25, 12, null);
+            } else if (type == 1) {
+                final BufferedImage image = ImageIO.read(new File("images\\ChromeDino_Cactus1.png"));
+                g.drawImage(image, p.getWidth() - 30 - x, p.getHeight() / 2 - 20, 10, 20, null);
+            } else if (type == 2) {
+                final BufferedImage image = ImageIO.read(new File("images\\ChromeDino_Birb2.png"));
+                g.drawImage(image, p.getWidth() - 30 - x, p.getHeight() / 2 - 45, 20, 20, null);
+            } else if (type == 3) {
+                final BufferedImage image = ImageIO.read(new File("images\\ChromeDino_Birb1.png"));
+                g.drawImage(image, p.getWidth() - 30 - x, p.getHeight() / 2 - 25, 15, 15, null);
+            }
+        } catch (IOException ex){};
 
-        if (type==0)
-            g.fillRect(p.getWidth()-30-x,p.getHeight()/2-10,20,10);
-
-        else if (type == 1)
-            g.fillRect(p.getWidth()-30-x,p.getHeight()/2-20,10,20);
-        else if (type==2)
-            g.fillRect(p.getWidth()-30-x,p.getHeight()/2-35,10,10);
-        else if (type == 3)
-            g.fillRect(p.getWidth()-30-x,p.getHeight()/2-20,9,9);
 
 
     }
@@ -178,6 +204,3 @@ class cactus {
 
 
 }
-
-
-
